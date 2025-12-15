0b6bc6cbe567f581637c9ece6f16d4efbd75eb44
Alek Melenski <alekmelenski@Aleks-MacBook-Pro.local>
2025-12-15 03:08:52 -0500

    fixed initializing parameters based off of batchsize, added function to generate testing data for comparison, fixed loss and dloss computing only from first 100 labels, fixed training on the same first 100 labels for each epoch

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import numpy as np
 import pandas as pd
+import os
 
-data = pd.read_csv('/Users/alekmelenski/Desktop/NeuralNet/train.csv')
-testing = pd.read_csv('/Users/alekmelenski/Desktop/NeuralNet/test.csv')
+cwd = os.getcwd()
+
+data = pd.read_csv(os.path.join(cwd, "train.csv"))
+testing = pd.read_csv(os.path.join(cwd, "test.csv"))
 data = np.array(data)
+testing = np.array(testing)
 
 labels = data[:, 0]
 training = data[:, 1:]
 
-BATCHSIZE = 100
-inputs = training[0:BATCHSIZE]
-
 #np.random.seed(42)
 
-def init_params(batchsize, neurons, neurons2):
+def init_params(neurons, neurons2):
     W1 = np.random.uniform(-0.01, 0.01, (784, neurons))   # weights from input → hidden
-    b1 = np.random.uniform(-0.5, 0.5, (batchsize, neurons))   # biases for hidden layer
+    b1 = np.random.uniform(-0.5, 0.5, (1, neurons))   # biases for hidden layer
     W2 = np.random.uniform(-0.01, 0.01, (neurons, neurons2))   # weights from hidden → output, output is neurons 2
-    b2 = np.random.uniform(-0.5, 0.5, (batchsize, neurons2))   # biases for output layer
+    b2 = np.random.uniform(-0.5, 0.5, (1, neurons2))   # biases for output layer
     return W1, b1, W2, b2
     
 def ReLU(x):
@@ -36,9 +37,13 @@ def forward_prop(W1, b1, W2, b2, input):
     return Z1, A1, Z2, y
 
 def loss(y, labels):
+    batch_size = len(labels)
+
     y = np.clip(y, 1e-7, 1 - 1e-7) # Sets a range so you dont take the log of 0 (inf)
-    loss = -np.log(y[range(len(y)), labels[:BATCHSIZE]]) #categorical cross entropy (-log) | selects the index of the correct label for each row (batch)
+
+    loss = -np.log(y[range(len(labels)), labels]) #categorical cross entropy (-log) | selects the index of the correct label for each row (batch)
     cost = np.mean(loss)
+
     return cost
 
 def dLoss(y ,labels):
@@ -49,12 +54,12 @@ def dLoss(y ,labels):
     Formula: predicted - groud truth
     '''
     
-    one_hot = np.eye(10)[labels[:BATCHSIZE]] #One hot encode labels
-
-    #Gradient
+    one_hot = np.eye(10)[labels] #One hot encode labels
+                                 #one_hot = np.eye(10)[labels[:BATCHSIZE]]
+    #Gradient                       ^Interesting to note that this still works somehow
     dLoss = y - one_hot
     #Normalize Gradient
-    dLoss_norm = dLoss / BATCHSIZE
+    dLoss_norm = dLoss / len(labels)
 
     return dLoss_norm
 
@@ -73,23 +78,29 @@ def dReLU(dvalues, inputs):
 
 def accuracy(y, labels):
     predictions = np.argmax(y , axis=1)
-    return np.mean(predictions == labels[:BATCHSIZE])
+    return np.mean(predictions == labels)
         
-W1, b1, W2, b2 = init_params(BATCHSIZE, 10, 10)
+W1, b1, W2, b2 = init_params(10, 10)
 
-epochs = 100
+epochs = 10000
 LEARNING_RATE = 0.001
+BATCHSIZE = 500
+
 for epoch in range(epochs):
     print(f"\n=== Epoch {epoch+1} ===")
     
+    indicies = np.random.randint(0, len(training), size = BATCHSIZE)
+    training_batch = training[indicies]
+    labels_batch = labels[indicies]
+
     # Forward pass
-    Z1, A1, Z2, y = forward_prop(W1, b1, W2, b2, inputs)
+    Z1, A1, Z2, y = forward_prop(W1, b1, W2, b2, training_batch)
     
     # Backward pass
-    dLoss_output = dLoss(y, labels)
+    dLoss_output = dLoss(y, labels_batch)
     dWeight2, dBias2, dInput2 = dLayer(A1, W2, dLoss_output)
     dReLU_output = dReLU(dInput2, Z1)
-    dWeight1, dBias1, dInput1 = dLayer(inputs, W1, dReLU_output)
+    dWeight1, dBias1, dInput1 = dLayer(training_batch, W1, dReLU_output)
     
     # Gradient descent update
     W1 -= LEARNING_RATE * dWeight1
@@ -98,6 +109,23 @@ for epoch in range(epochs):
     b2 -= LEARNING_RATE * dBias2
     
     # Print progress
-    current_loss = loss(y, labels)
-    current_accuracy = accuracy(y, labels)
+    current_loss = loss(y, labels_batch)
+    current_accuracy = accuracy(y, labels_batch)
     print(f"Loss: {current_loss:.7f}, Accuracy: {current_accuracy:.4f}")
+
+
+def kaggleTest(testing):
+    
+    _, _, _, predicted = forward_prop(W1, b1, W2, b2, testing)
+
+    predicted = np.argmax(predicted, axis = 1)
+
+    predictedDf = pd.DataFrame({
+        "ImageId": np.arange(1, len(predicted) + 1),
+        "Label": predicted
+        })
+    
+    print(predictedDf)
+    predictedDf.to_csv("submission.csv", index=False)
+
+#kaggleTest(testing=testing)
